d8f3a6271d9bb596e67b36e5ae7d10ca07a547da
nonokEE <shshrdl@naver.com>
2022-03-30 02:02:30 +0900

    click and get patch for 1st.jpg

--- test.py
+++ test.py
@@ -3,22 +3,28 @@ import matplotlib.image as img
 import matplotlib.pyplot as plt
 import numpy as np
 
-dir_del = None
-clicked_points = []
+coord1 = []
+coord2 = []
 
-def getCoord(event, x, y, flags, param):
+def getCoord_1(event, x, y, flags, param):
     if event == cv2.EVENT_LBUTTONDOWN:
+        coord1.append((y,x))
         print(x, y)
-
 
 image1_cv = cv2.imread("1st.jpg")
 
 cv2.namedWindow("1st", cv2.WINDOW_NORMAL)
 cv2.resizeWindow("1st", 1000, 800)
-cv2.setMouseCallback("1st", getCoord)
+cv2.setMouseCallback("1st", getCoord_1)
 
 cv2.imshow("1st", image1_cv)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
-print(clicked_points)
+cv2.namedWindow("1st_1", cv2.WINDOW_NORMAL)
+cv2.resizeWindow("1st_1", 500, 500)
+cv2.setMouseCallback("1st_1", getCoord_1)
+
+cv2.imshow("1st_1", image1_cv[coord1[0][0]-10:coord1[0][0]+10, coord1[0][1]-10:coord1[0][1]+10])
+cv2.waitKey(0)
+cv2.destroyAllWindows()
