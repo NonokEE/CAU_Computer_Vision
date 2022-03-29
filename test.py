5706b4fc38a2f30c3cd094c82b215f0ee5bb9e49
nonokEE <shshrdl@naver.com>
2022-03-30 01:29:03 +0900

    test: get coord on image

--- test.py
+++ test.py
@@ -1,12 +1,24 @@
 import cv2
-
 import matplotlib.image as img
 import matplotlib.pyplot as plt
+import numpy as np
 
-image1 = img.imread("1st.jpg")
+dir_del = None
+clicked_points = []
+
+def getCoord(event, x, y, flags, param):
+    if event == cv2.EVENT_LBUTTONDOWN:
+        print(x, y)
+
+
 image1_cv = cv2.imread("1st.jpg")
 
+cv2.namedWindow("1st", cv2.WINDOW_NORMAL)
+cv2.resizeWindow("1st", 1000, 800)
+cv2.setMouseCallback("1st", getCoord)
 
-plt.figure()
-plt.imshow(image1_cv)
-plt.show()
+cv2.imshow("1st", image1_cv)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+print(clicked_points)
